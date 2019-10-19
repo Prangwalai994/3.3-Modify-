fffc38c60fa1eb645beaa478eb19610dc4c35d5d
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-19 19:28:23 +0700

    3_3 Modify (2)

--- MathUnitTest3_3/UnitTest1.cs
+++ MathUnitTest3_3/UnitTest1.cs
@@ -40,6 +40,19 @@ namespace MathUnitTest3_3
 
             Assert.That(result, Is.EqualTo(expectedResult));
         }
-        
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit()
+        {
+            var result = _math.GetOddNumbers(5);
+
+            //Assert.That(result, Is.Not.Empty);
+
+            //Assert.That(result.Count(), Is.EqualTo(3));
+
+            //Assert.That(result, Does.Contain(1));
+            //Assert.That(result, Does.Contain(3));
+            //Assert.That(result, Does.Contain(5));
+            Assert.That(result, Is.EquivalentTo(new[] { 1, 3, 5 }));
+        }
     }
 }
